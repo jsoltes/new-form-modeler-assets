4a2d6a0fa9db06fef168b7688f5fc7155dccfbd2
testadmin <>
2017-01-24 14:38:00 +0100

    {/testproject/src/main/java/myteam/testproject/Person.java}

--- testproject/src/main/java/myteam/testproject/Person.java
+++ testproject/src/main/java/myteam/testproject/Person.java
@@ -5,31 +5,62 @@ package myteam.testproject;
  */
 
 @javax.persistence.Entity
-public class Person implements java.io.Serializable {
+public class Person implements java.io.Serializable
+{
 
-    static final long serialVersionUID = 1L;
+   static final long serialVersionUID = 1L;
 
-    @javax.persistence.GeneratedValue(strategy = javax.persistence.GenerationType.AUTO, generator = "PERSON_ID_GENERATOR")
-    @javax.persistence.Id
-    @javax.persistence.SequenceGenerator(sequenceName = "PERSON_ID_SEQ", name = "PERSON_ID_GENERATOR")
-    private java.lang.Long id;
+   @javax.persistence.GeneratedValue(strategy = javax.persistence.GenerationType.AUTO, generator = "PERSON_ID_GENERATOR")
+   @javax.persistence.Id
+   @javax.persistence.SequenceGenerator(sequenceName = "PERSON_ID_SEQ", name = "PERSON_ID_GENERATOR")
+   private java.lang.Long id;
 
-    public Person() {
-    }
-    
-    public Person(java.lang.Long id) {
-        this.id = id;
-    }
+   @org.kie.api.definition.type.Label(value = "name")
+   private java.lang.String name;
 
-    public java.lang.Long getId() {
-        return this.id;
-    }
-    
-    public void setId(java.lang.Long id) {
-        this.id = id;
-    }
+   @org.kie.api.definition.type.Label(value = "surname")
+   private java.lang.String surname;
 
+   public Person()
+   {
+   }
 
+   public java.lang.Long getId()
+   {
+      return this.id;
+   }
 
+   public void setId(java.lang.Long id)
+   {
+      this.id = id;
+   }
+
+   public java.lang.String getName()
+   {
+      return this.name;
+   }
+
+   public void setName(java.lang.String name)
+   {
+      this.name = name;
+   }
+
+   public java.lang.String getSurname()
+   {
+      return this.surname;
+   }
+
+   public void setSurname(java.lang.String surname)
+   {
+      this.surname = surname;
+   }
+
+   public Person(java.lang.Long id, java.lang.String name,
+         java.lang.String surname)
+   {
+      this.id = id;
+      this.name = name;
+      this.surname = surname;
+   }
 
 }
